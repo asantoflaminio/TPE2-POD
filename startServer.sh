df9c0cd9d29e2aaa67ca328033bb1f654005dc2f
Alejandro <asantoflaminio@itba.edu.ar>
2019-10-29 13:44:52 -0300

    start servers.sh

--- startServer.sh
+++ startServer.sh
@@ -1,6 +1,21 @@
-#!/bin/bash
-mkdir server/target/tp-server-1.0-SNAPSHOT
-tar -xzf server/target/tp-server-1.0-SNAPSHOT-bin.tar.gz -C server/target
-chmod +x server/target/tp-server-1.0-SNAPSHOT/run-server.sh
-cd server/target/tp-server-1.0-SNAPSHOT
-./run-server.sh
+#!/bin/sh
+mvn clean install
+cd podtpe
+cd server
+cd target
+tar -xzvf tp-server-1.0-SNAPSHOT-bin.tar
+pwd
+cd tp-server-1.0-SNAPSHOT
+chmod +x run-registry.sh
+chmod +x run-server.sh
+cd ..
+cd ..
+cd ..
+cd client
+cd target
+tar -xzvf podtpe-client-1.0-SNAPSHOT-bin.tar.gz
+cd podtpe-client-1.0-SNAPSHOT
+chmod +x run-managementclient.sh
+chmod +x run-fiscalclient.sh
+chmod +x run-queryclient.sh
+chmod +x run-votingclient.sh
