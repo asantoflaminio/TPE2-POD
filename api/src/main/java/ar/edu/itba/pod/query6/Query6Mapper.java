7000ffb594cca8d0007b11e89620acd12eb6cc03
asantoflaminio <asantoflaminio@itba.edu.ar>
2019-10-30 12:23:35 -0300

    query6 api stuff

---
+++ api/src/main/java/ar/edu/itba/pod/query6/Query6Mapper.java
@@ -0,0 +1,29 @@
+package ar.edu.itba.pod.query6;
+
+import java.util.Map;
+
+import com.hazelcast.mapreduce.Context;
+import com.hazelcast.mapreduce.Mapper;
+
+import ar.edu.itba.pod.Movement;
+import ar.edu.itba.pod.Pair;
+
+public class Query6Mapper implements Mapper<String, Movement, Pair<String, String>, Integer> {
+	
+    private final Map<String, String> mapOaciState;
+
+    public Query6Mapper(Map<String, String> map) {
+        this.mapOaciState = map;
+    }
+
+    @Override
+    public void map(String s, Movement movement, Context<Pair<String,String>, Integer> context) {
+
+        String origin= mapOaciState.get(movement.getSourceOACI());
+        String destination = mapOaciState.get(movement.getDestinationOACI());
+
+        if(origin != null && destination != null && !origin.equalsIgnoreCase(destination)) {
+            context.emit(new Pair<String, String>(origin, destination), 1);
+        }
+}
+}
---
+++ api/src/main/java/ar/edu/itba/pod/query6/Query6ReducerFactory.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.pod.query6;
+
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
+
+import ar.edu.itba.pod.Pair;
+
+public class Query6ReducerFactory implements ReducerFactory<Pair<String,String>, Integer, Integer>{
+	
+	@Override
+    public Reducer<Integer, Integer> newReducer(Pair<String,String> pair) {
+        return new Query6Reducer();
+    }
+
+    private class Query6Reducer extends Reducer<Integer, Integer> {
+    	
+        private volatile int accum;
+
+        @Override
+        public void beginReduce() {
+            accum = 0;
+        }
+
+        @Override
+        public void reduce(Integer integer) {
+            accum += integer;
+        }
+
+        @Override
+        public Integer finalizeReduce() {
+            return accum;
+        }
+}
+}
